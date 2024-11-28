9541056aa8956f88d71a18f9a958a21ec144c2a6
Armandas Lidžius <armandaslid@gmail.com>
2024-11-28 15:16:49 +0200

    Update TravelTide_SQL_Code.sql

--- TravelTide_Files/TravelTide_SQL_Code.sql
+++ TravelTide_Files/TravelTide_SQL_Code.sql
@@ -1,7 +1,7 @@
 WITH session_based AS
 (
 SELECT	s.session_id
-				,s.user_id
+	,s.user_id
         ,s.trip_id
         ,s.session_start
         ,s.session_end
@@ -48,12 +48,12 @@ WHERE s.user_id IN (SELECT	user_id
 user_based AS
 (
 SELECT	user_id
-				,SUM(page_clicks) AS num_clicks
+	,SUM(page_clicks) AS num_clicks
         ,COUNT(distinct session_id) AS num_sessions
         ,ROUND(AVG(session_duration), 2) AS avg_session_duration
-				,(SUM(CASE WHEN flight_booked OR hotel_booked THEN 1 ELSE 0 END)::FLOAT / COUNT(session_id)) * 100 AS booking_rate
-				,(SUM(CASE WHEN cancellation THEN 1 ELSE 0 END)::FLOAT / COUNT(session_id)) * 100 AS cancellation_rate
-				,ROUND(AVG(page_clicks), 2) AS avg_clicks_per_session
+	,(SUM(CASE WHEN flight_booked OR hotel_booked THEN 1 ELSE 0 END)::FLOAT / COUNT(session_id)) * 100 AS booking_rate
+	,(SUM(CASE WHEN cancellation THEN 1 ELSE 0 END)::FLOAT / COUNT(session_id)) * 100 AS cancellation_rate
+	,ROUND(AVG(page_clicks), 2) AS avg_clicks_per_session
 				
 FROM session_based
 GROUP BY user_id
@@ -62,27 +62,27 @@ GROUP BY user_id
 trip_based AS
 (
 SELECT	user_id
-				,COUNT(trip_id) AS num_trips
+	,COUNT(trip_id) AS num_trips
         ,SUM(CASE WHEN flight_booked AND return_flight_booked THEN 2
              			WHEN flight_booked THEN 1
              			ELSE 0
              END) AS num_flights
-				,SUM((hotel_price_per_room_night_usd * nights * rooms) * (1 - COALESCE(hotel_discount_amount, 0))) AS money_spent_hotel
+	,SUM((hotel_price_per_room_night_usd * nights * rooms) * (1 - COALESCE(hotel_discount_amount, 0))) AS money_spent_hotel
         ,ROUND(AVG(EXTRACT(DAY FROM departure_time - session_end)), 0) AS avg_days_before_trip
         ,ROUND(AVG(HAVERSINE_DISTANCE(home_airport_lat, home_airport_lon, destination_airport_lat, destination_airport_lon))::NUMERIC, 0) AS avg_km_flown
-				,SUM(base_fare_usd * (1 - COALESCE(flight_discount_amount, 0))) AS money_spent_flight
-				,(SUM((base_fare_usd * (1 - COALESCE(flight_discount_amount, 0)))
+	,SUM(base_fare_usd * (1 - COALESCE(flight_discount_amount, 0))) AS money_spent_flight
+	,(SUM((base_fare_usd * (1 - COALESCE(flight_discount_amount, 0)))
               +
               (hotel_price_per_room_night_usd * nights * rooms * (1 - COALESCE(hotel_discount_amount, 0))))
-          		/
-          		NULLIF(COUNT(trip_id), 0)) AS avg_spending_per_trip
-				,ABS(ROUND(AVG(EXTRACT(EPOCH FROM (check_out_time - check_in_time)) / 86400), 0)) AS avg_trip_duration_days
-				,(SUM(CASE WHEN flight_booked AND hotel_booked THEN 1 ELSE 0 END)::FLOAT / COUNT(trip_id)) * 100 AS flight_hotel_combo_rate
-				,ROUND(AVG(seats), 0) AS avg_seats_per_flight
-				,(SUM(CASE WHEN flight_discount OR hotel_discount THEN 1 ELSE 0 END)::FLOAT / COUNT(trip_id)) * 100 AS discount_utilization_rate
-				,AVG(flight_discount_amount) AS avg_flight_discount
-				,MAX(HAVERSINE_DISTANCE(home_airport_lat, home_airport_lon, destination_airport_lat, destination_airport_lon)) AS max_distance_traveled
-				---,
+          	/
+          	NULLIF(COUNT(trip_id), 0)) AS avg_spending_per_trip
+	,ABS(ROUND(AVG(EXTRACT(EPOCH FROM (check_out_time - check_in_time)) / 86400), 0)) AS avg_trip_duration_days
+	,(SUM(CASE WHEN flight_booked AND hotel_booked THEN 1 ELSE 0 END)::FLOAT / COUNT(trip_id)) * 100 AS flight_hotel_combo_rate
+	,ROUND(AVG(seats), 0) AS avg_seats_per_flight
+	,(SUM(CASE WHEN flight_discount OR hotel_discount THEN 1 ELSE 0 END)::FLOAT / COUNT(trip_id)) * 100 AS discount_utilization_rate
+	,AVG(flight_discount_amount) AS avg_flight_discount
+	,MAX(HAVERSINE_DISTANCE(home_airport_lat, home_airport_lon, destination_airport_lat, destination_airport_lon)) AS max_distance_traveled
+	
 FROM session_based
 WHERE trip_id IS NOT NULL AND trip_id NOT IN (SELECT DISTINCT trip_id
                                               FROM session_based
@@ -91,7 +91,7 @@ GROUP BY user_id
 )
 
 SELECT	ub.*
-				,EXTRACT(YEAR FROM age(now(), u.birthdate)) AS age
+	,EXTRACT(YEAR FROM age(now(), u.birthdate)) AS age
         ,u.gender
         ,u.married
         ,u.has_children
